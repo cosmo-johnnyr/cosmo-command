a3c6df0884ca36dd74f65732a82a0cd70a7d9f27
cosmo-johnnyr <cosmo_johnnyr@icloud.com>
2026-02-04 22:38:26 -0600

    Update to latest API URL

--- cosmo-command/src/App.js
+++ cosmo-command/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 const PASSWORD = 'cosmocommand1';
 const AUTH_KEY = 'cosmo_auth';
-const API_URL = 'https://geology-jose-joined-magazine.trycloudflare.com';
+const API_URL = 'https://hormone-maiden-cost-trained.trycloudflare.com';
 const POLL_INTERVAL = 5000; // Poll every 5 seconds
 
 // Mock data for demo/fallback mode
